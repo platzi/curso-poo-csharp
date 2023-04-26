401b23c2110c8af9af006b5d1ccf7f5c308c3acf
Miguel Teheran <miguelteheran@gmail.com>
2023-04-26 18:18:49 -0500

    agregando clase superpoder

--- SuperHeroesApp/Program.cs
+++ SuperHeroesApp/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad para volar y planear en el aire";
+poderVolar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+superFuerza.Nombre = "Super fuerza";
+superFuerza.Nivel = NivelPoder.NivelTres;
+
 var superman = new SuperHeroe();
 
 superman.Id = 1;
@@ -8,7 +17,11 @@ superman.Nombre = "Superman";
 superman.IdentidadSecreta = "Clark Kent";
 superman.Ciudad = "Metropolis";
 superman.PuedeVolar = true;
-superman.SuperPoderes = new []{ "Vision de rayos X", "Volar", "Super fueza", "Ojos rayos lazer" };
+
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+poderesSuperman.Add(poderVolar);
+poderesSuperman.Add(superFuerza);
+superman.SuperPoderes = poderesSuperman;
 
 class SuperHeroe
 {
@@ -16,6 +29,20 @@ class SuperHeroe
     public string Nombre;
     public string IdentidadSecreta;
     public string Ciudad;
-    public string[] SuperPoderes;
+    public List<SuperPoder> SuperPoderes;
     public bool PuedeVolar;
+}
+
+class SuperPoder
+{
+    public string Nombre;
+    public string Descripcion;
+    public NivelPoder Nivel;
+}
+
+enum NivelPoder
+{
+    NivelUno,
+    NivelDos,
+    NivelTres
 }
